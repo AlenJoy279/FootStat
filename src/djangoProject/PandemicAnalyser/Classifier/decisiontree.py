bcbc15023489b4aab07c60bf10f8bf051d14c51d
Jaime <jaime.deviverowoods2@mail.dcu.ie>
2023-03-29 00:22:42 +0100

    Predictor models evaluation
    Made classifier and clustering models and measured the accuracy of each
    model for predicting tweet sentiment.

--- src/djangoProject/PandemicAnalyser/Classifier/decisiontree.py
+++ src/djangoProject/PandemicAnalyser/Classifier/decisiontree.py
@@ -1,8 +1,8 @@
 from textblob import TextBlob
-from textblob.classifiers import NaiveBayesClassifier
+from textblob.classifiers import DecisionTreeClassifier
 
 #11
-train = [("Happy Lockdown 6.0! - Drinking a Triple Tripel Straight Up by @thehowlinghound @ Bringabeeralong Holiday House", "pos"),
+train2 = [("Happy Lockdown 6.0! - Drinking a Triple Tripel Straight Up by @thehowlinghound @ Bringabeeralong Holiday House", "pos"),
         ("INNR \u2014 COVID likely to lock India's women out of job market for years: Azim Premji University report finds 47 percent of women who lost jobs last year have been made permanently redundant. A woman sweeps next to the\u00a0... View article... https://t.co/WnOv8JLSpX", "neg"),
         ("Your prayers have worked again and Ad\u00e8le is getting better by the day. Thank you so much for thinking of Ad\u00e8le in her time of need. #covid #friends #love #people #europe #nuskin #family @ Billings, Montana https://t.co/5iTq6UuKcT", "pos"),
         ("So excited for this event.. come out in support me and all the other businesses coming \ud83e\udd17.\n#bakerdaves #eventcoming @ Corona, California https://t.co/sBsXKHgnfx", "pos"),
@@ -36,11 +36,11 @@ train = [("Happy Lockdown 6.0! - Drinking a Triple Tripel Straight Up by @thehow
          # POSITIVE FAKE DATA MADE BY ME
         ("Had a very nice time today at the park", 'pos'),
         ("Lovely time today with friends. It makes me happy to talk to you guys ", 'pos'),
-        ("So happy to have this opportunity to work at Microsoft", 'pos'),
-        ("Nothing better than reuniting with good old friends", 'pos'),
-        ("I am relieved to see all my friends do well", 'pos'),
-        ("Had great fun today at this Zoom event held to motivate people to exercise. Great work everyone!", 'pos'),
-        ("There is such wonderful scenery at the Cliffs of Moher", 'pos'),
+        # ("So happy to have this opportunity to work at Microsoft", 'pos'),
+        # ("Nothing better than reuniting with good old friends", 'pos'),
+        # ("I am relieved to see all my friends do well", 'pos'),
+        # ("Had great fun today at this Zoom event held to motivate people to exercise. Great work everyone!", 'pos'),
+        # ("There is such wonderful scenery at the Cliffs of Moher", 'pos'),
         ("This new Netflix show is so fun!! I like it a lot", 'pos'),
         ("Gorgeous views at Dublin today", 'pos'),
         ("What a cool new feature added to Call Of Duty !", 'pos'),
@@ -49,6 +49,11 @@ train = [("Happy Lockdown 6.0! - Drinking a Triple Tripel Straight Up by @thehow
         ("Happy Birthday Jake, let's have a big celebration of your 40 years after lockdown ends", 'pos'),
         ("I'm so lucky to have such thoughtful and kind friends. Thank you to everyone", 'pos'),
         ("This new Fortnite skin is so impressive. I LOVE it, make more amazing content like this!", 'pos'),
+        ("I love sweets", "pos"),
+        ("My family give me such great support", "pos"),
+        ("Games are so enjoyable", "pos"),
+        ("So special to me! I cherish these memories so much", "pos"),
+        ("I'm so happy to spend my time in sunny weather", "pos"),
          # NEGATIVE FAKE DATA MADE BY ME
         ("I hate being locked down", 'neg'),
         ("Such terrible news came out today. I am so sad", 'neg'),
@@ -59,7 +64,12 @@ train = [("Happy Lockdown 6.0! - Drinking a Triple Tripel Straight Up by @thehow
         ("The mobile app is glitchy and definitely not user friendly", 'neg'),
         ("I HATE this game", 'neg'),
         ("Ranger fans are terrible!", 'neg'),
-        ("Horrible comments made by the President today", 'neg')
+        ("Horrible comments made by the President today", 'neg'),
+        ("I hate cheesecake", 'neg'),
+        ("I am so exhausted, I hate work", 'neg'),
+        ("The UFC is disgusting and  barbaric", 'neg'),
+        ("Our government is corrupt and evil", 'neg'),
+        ("Most people are fake, ignorant and ill-intentioned", 'neg')
          ]
 
 test = [("Great day of catching up with dear friends I haven\u2019t seen since b4 these strange crazy pandemic times \ud83d\udc9c\n\n#postpandemictimes \n#jw @ Zelda's Restaurant-Capitola https://t.co/6CGrPCyxFB", 'pos'),
@@ -100,67 +110,12 @@ test = [("Great day of catching up with dear friends I haven\u2019t seen since b
         ("Peace and Love to everyone! Let's unite and be in harmony", 'pos')
         ]
 
-xtest = ["Great day of catching up with dear friends I haven\u2019t seen since b4 these strange crazy pandemic times \ud83d\udc9c\n\n#postpandemictimes \n#jw @ Zelda's Restaurant-Capitola https://t.co/6CGrPCyxFB",
-        "The Saddest News Of The Decade...!!!!\ud83e\udd7a\nShri Milkha Singh Just Passed Away At The Age Of 91 Due To Covid 19 ..\ud83d\ude4f\ud83c\udffc\nRIP To The Olympic Hero Of India..\ud83d\ude22\nMay Your Sole Rest In Peace Sir..\ud83d\ude4f\ud83c\udffc\n\n#TheAmateurBong https://t.co/ycetcfdJKN",
-        "This some real Bull \ud83d\udca9 don't make me move back to Texas where Vaccine Passports are banned. @ Los Angeles, California https://t.co/XDL7S6vs9Q",
-        "So grateful to everyone who made this happen! \ud83d\ude42\ud83d\udc4d\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n#OneAccenture #We4Vaccine #Vaccination #COVID_19 @ Accenture, Magarpatta City, Pune https://t.co/Mz1scHmCsP",
-        "You can't see it, but I'm wearing the biggest smile \ud83d\udc89. \n\n#Sinovac #1stDoseDone #COVID19 @ Kuala Lumpur, Malaysia https://t.co/m08GIx70F8",
-        "How Covid-19 fucked me up and today I am again at where I was ..lost all muscles, lost the 6 packs abs. #memories2019 \nWish I could go back to being lean and mean.. once again. @ Pune, Maharashtra https://t.co/t50q8smqLh",
-        "Fuck COVID-19 \ud83d\udc4a\ud83d\udc4a\ud83d\udc4a\ud83e\uddbe\n\nVaccinated. #covid_19 #fuckcovid19 @ Miami, Florida https://t.co/SSvIZYvol6",
-        "My brother and I took my dad out for dinner for the first time since the pandemic started. It was so nice to finally see my dad after a long year. #\u0262\u1d07\u1d1b\u1d20\u1d00\u1d04\u1d04\u026a\u0274\u1d00\u1d1b\u1d07\u1d05 @ Ke'e Grill https://t.co/6e9lOrM3KL",
-        "This is crazy.  First COVID now Gas Prices!!! @ Florence, South Carolina https://t.co/hliwmzpVgT",
-        "Celebrating being back after another lockdown with this beauty! \ud83e\udde1 @ Emma Frances Hair https://t.co/Easvp9M4Vr",
-        "Finally got to hug my Mom after over a year of social distancing! Yay! #ilovemymomanddad @ Roseville, California https://t.co/cel0jJ7vWL",
-        "The biggest thing I have missed because of Covid 19. Football with @christopherwilliams308 @ Sutton United F.C. https://t.co/tpSynbfT8F",
-        "Gorgeous sunset tonight while out grabbing dinner!! #SoCal #Sunset #Corona @ Subway https://t.co/w81i5MNQeJ",
-        "The former guy only cares for himself https://t.co/bly2njXRuL",
-        "LOVE THIS!!! #itsallcomingback #itswhatwedo #covid_19 @ Funkbox Productions https://t.co/CofmRJe9xW",
-        "Watching these pictures now makes me feel as if it was a hundred years ago. My personal remedy from the lockdown stressed mood. \ud83c\uddeb\ud83c\uddf7 \n#paris #france #theeiffeltower #memories #throwbackmonday (Tour Eiffel): https://t.co/0f7mgPMC4U",
-         # FAKE TEST DATA MADE BY ME:
-         "This pandemic is such a scam! Down with the government!",
-         "I hate these restrictions, I feel like my time is being wasted",
-         "Love to see people finding ways to stay connected, we'll get through this together!",
-         "So happy to have visited my sister, I hadn't seen her in 3 months. Love you sis",
-         "I am disgusted at people not wearing masks, our hospitals are full, do your part!",
-         "I hate how Nike charges over 100$ for any popular sneaker",
-         "What an awful show keeping up with the Kardashains is... We need better TV",
-         "Many Thanks to the @Nike family for the gift. Looking forward to future collaborations with you guys",
-         "Proud to be nominated for this award amongst so many other great competitors",
-         "Had an mazing experience today at Tayto Park. The rides are so exciting and the kids loved it! I recommended to anyone planning a day out",
-         "The food at McDonalds is disgusting... Im never eating there again",
-         "Our president is pathetic. We NEED new elections"
-         "Our president is the best we have ever had",
-         "Life at Riyadh is excellent. Everything is luxurious",
-         "Our country is in an awful state",
-         "We need more government aid. Our country is in ruins",
-         "My greatest success up to has been my resilience. Lets all keep going",
-         "Stay strong people! You are tougher than you think",
-         "Peace and Love to everyone! Let's unite and be in harmony"
-        ]
+#### DECISION TREE CLASSIFIER #####
+dtc = DecisionTreeClassifier(train2) # train decision tree algorithm using training data
 
+print("\nOVERALL Decision Tree Classifier ACCURACY: " + str(dtc.accuracy(test)) + "\n") # print accuracy using test set
 
-cl = NaiveBayesClassifier(train)
-
-print("\nOVERALL Naive Bayes Classifier ACCURACY: " + str(cl.accuracy(test)))
-
-# for s in xtest:
-#       blob = TextBlob(s)
-      #print("BUILT-IN SENTIMENT: " + str(blob.sentiment.polarity))
-      #print("BAYES CLASSIFIER: " + str(cl.classify(blob)))
-
-correct = 0
 for s in test:
-        blob = TextBlob(s[0])
-        if blob.sentiment.polarity < 0.0:
-                score = 'neg'
-        else:
-                score = 'pos'
-
-        if score == s[1]:
-                correct += 1
-
-accuracy = correct / len(test)
-print("\nOVERALL Built-in Polarity function ACCURACY: " + str(accuracy) + "\n")
-
-def get_classification(train,test ): # given a list of training data
-    pass
+      blob = TextBlob(s[0])
+      print("BUILT-IN SENTIMENT: " + str(blob.sentiment.polarity))
+      print("DECISION TREE CLASSIFIER: " + str(dtc.classify(blob)) + "\n")
--- src/djangoProject/PandemicAnalyser/Classifier/kmeans.py
+++ src/djangoProject/PandemicAnalyser/Classifier/kmeans.py
@@ -1,5 +1,7 @@
 from textblob import TextBlob
-from textblob.classifiers import NaiveBayesClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cluster import KMeans
+
 
 #11
 train = [("Happy Lockdown 6.0! - Drinking a Triple Tripel Straight Up by @thehowlinghound @ Bringabeeralong Holiday House", "pos"),
@@ -100,67 +102,40 @@ test = [("Great day of catching up with dear friends I haven\u2019t seen since b
         ("Peace and Love to everyone! Let's unite and be in harmony", 'pos')
         ]
 
-xtest = ["Great day of catching up with dear friends I haven\u2019t seen since b4 these strange crazy pandemic times \ud83d\udc9c\n\n#postpandemictimes \n#jw @ Zelda's Restaurant-Capitola https://t.co/6CGrPCyxFB",
-        "The Saddest News Of The Decade...!!!!\ud83e\udd7a\nShri Milkha Singh Just Passed Away At The Age Of 91 Due To Covid 19 ..\ud83d\ude4f\ud83c\udffc\nRIP To The Olympic Hero Of India..\ud83d\ude22\nMay Your Sole Rest In Peace Sir..\ud83d\ude4f\ud83c\udffc\n\n#TheAmateurBong https://t.co/ycetcfdJKN",
-        "This some real Bull \ud83d\udca9 don't make me move back to Texas where Vaccine Passports are banned. @ Los Angeles, California https://t.co/XDL7S6vs9Q",
-        "So grateful to everyone who made this happen! \ud83d\ude42\ud83d\udc4d\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n#OneAccenture #We4Vaccine #Vaccination #COVID_19 @ Accenture, Magarpatta City, Pune https://t.co/Mz1scHmCsP",
-        "You can't see it, but I'm wearing the biggest smile \ud83d\udc89. \n\n#Sinovac #1stDoseDone #COVID19 @ Kuala Lumpur, Malaysia https://t.co/m08GIx70F8",
-        "How Covid-19 fucked me up and today I am again at where I was ..lost all muscles, lost the 6 packs abs. #memories2019 \nWish I could go back to being lean and mean.. once again. @ Pune, Maharashtra https://t.co/t50q8smqLh",
-        "Fuck COVID-19 \ud83d\udc4a\ud83d\udc4a\ud83d\udc4a\ud83e\uddbe\n\nVaccinated. #covid_19 #fuckcovid19 @ Miami, Florida https://t.co/SSvIZYvol6",
-        "My brother and I took my dad out for dinner for the first time since the pandemic started. It was so nice to finally see my dad after a long year. #\u0262\u1d07\u1d1b\u1d20\u1d00\u1d04\u1d04\u026a\u0274\u1d00\u1d1b\u1d07\u1d05 @ Ke'e Grill https://t.co/6e9lOrM3KL",
-        "This is crazy.  First COVID now Gas Prices!!! @ Florence, South Carolina https://t.co/hliwmzpVgT",
-        "Celebrating being back after another lockdown with this beauty! \ud83e\udde1 @ Emma Frances Hair https://t.co/Easvp9M4Vr",
-        "Finally got to hug my Mom after over a year of social distancing! Yay! #ilovemymomanddad @ Roseville, California https://t.co/cel0jJ7vWL",
-        "The biggest thing I have missed because of Covid 19. Football with @christopherwilliams308 @ Sutton United F.C. https://t.co/tpSynbfT8F",
-        "Gorgeous sunset tonight while out grabbing dinner!! #SoCal #Sunset #Corona @ Subway https://t.co/w81i5MNQeJ",
-        "The former guy only cares for himself https://t.co/bly2njXRuL",
-        "LOVE THIS!!! #itsallcomingback #itswhatwedo #covid_19 @ Funkbox Productions https://t.co/CofmRJe9xW",
-        "Watching these pictures now makes me feel as if it was a hundred years ago. My personal remedy from the lockdown stressed mood. \ud83c\uddeb\ud83c\uddf7 \n#paris #france #theeiffeltower #memories #throwbackmonday (Tour Eiffel): https://t.co/0f7mgPMC4U",
-         # FAKE TEST DATA MADE BY ME:
-         "This pandemic is such a scam! Down with the government!",
-         "I hate these restrictions, I feel like my time is being wasted",
-         "Love to see people finding ways to stay connected, we'll get through this together!",
-         "So happy to have visited my sister, I hadn't seen her in 3 months. Love you sis",
-         "I am disgusted at people not wearing masks, our hospitals are full, do your part!",
-         "I hate how Nike charges over 100$ for any popular sneaker",
-         "What an awful show keeping up with the Kardashains is... We need better TV",
-         "Many Thanks to the @Nike family for the gift. Looking forward to future collaborations with you guys",
-         "Proud to be nominated for this award amongst so many other great competitors",
-         "Had an mazing experience today at Tayto Park. The rides are so exciting and the kids loved it! I recommended to anyone planning a day out",
-         "The food at McDonalds is disgusting... Im never eating there again",
-         "Our president is pathetic. We NEED new elections"
-         "Our president is the best we have ever had",
-         "Life at Riyadh is excellent. Everything is luxurious",
-         "Our country is in an awful state",
-         "We need more government aid. Our country is in ruins",
-         "My greatest success up to has been my resilience. Lets all keep going",
-         "Stay strong people! You are tougher than you think",
-         "Peace and Love to everyone! Let's unite and be in harmony"
-        ]
+##### K-Means Clustering ####
 
+vectorizer = CountVectorizer() # initialize vectorizer to transform training and test sets to matrices
 
-cl = NaiveBayesClassifier(train)
+train_text = []
+for data in train: # iterate over training data and add it to list to be transformed to matrix
+    train_text.append(data[0]) # just use the text part of the tuple, which is first value, hence data[0]
 
-print("\nOVERALL Naive Bayes Classifier ACCURACY: " + str(cl.accuracy(test)))
+X_train = vectorizer.fit_transform(train_text) # convert the training data to a matrix
 
-# for s in xtest:
-#       blob = TextBlob(s)
-      #print("BUILT-IN SENTIMENT: " + str(blob.sentiment.polarity))
-      #print("BAYES CLASSIFIER: " + str(cl.classify(blob)))
+kmeans = KMeans(n_clusters=2) # 2 clusters has best performance
+kmeans.fit(X_train) # train K-means model
+
+test_text = []
+for data in test: # iterate over test data to add to list which will be transformed to matrix
+    test_text.append(data[0]) # add all text values without the pos or neg values to an array
+
+X_test = vectorizer.transform(test_text) # transform intro matrix
+predicted_labels = kmeans.predict(X_test) # predict clusters from test data
+
+scores = []
+for data in test: # iterate over data in test and add scores to list
+    if data[1] == "pos":
+        scores.append(1)
+    else:
+        scores.append(0)
 
 correct = 0
-for s in test:
-        blob = TextBlob(s[0])
-        if blob.sentiment.polarity < 0.0:
-                score = 'neg'
-        else:
-                score = 'pos'
+i = 0
+while i < len(predicted_labels):
+        #print(str(predicted_labels[i]) + " " + str(scores[i]))
+        if predicted_labels[i] == scores[i]: # if value correctly predicted ( the values are equal)
+                correct += 1 # if correct prediction add 1 to counter
+        i += 1
 
-        if score == s[1]:
-                correct += 1
-
-accuracy = correct / len(test)
-print("\nOVERALL Built-in Polarity function ACCURACY: " + str(accuracy) + "\n")
-
-def get_classification(train,test ): # given a list of training data
-    pass
+accuracy = (correct / len(predicted_labels))
+print("\nOVERALL K-means ACCURACY: " + str(accuracy))
