4688ff28b54941c1ef2fb164cea08b06ff01c57a
Jaime <jaime.deviverowoods2@mail.dcu.ie>
2023-04-21 18:13:48 +0100

    Backend
    1. Added 250 tweets to training data and 100 tweets to test data
    
    2. Made data.py and cleaned up duplicate code of training and test data

--- src/djangoProject/PandemicAnalyser/Graphs/barchart.py
+++ src/djangoProject/PandemicAnalyser/Graphs/barchart.py
@@ -36,10 +36,10 @@ def barchart_by_month(dict):
     return mpld3.fig_to_html(fig)
 
 
-def barchart_models(bayes, dt, km, knn, txtblob):
+def barchart_models(bayes, dt, km, knn, lr, txtblob):
 
-    labels = ['Naive Bayes Classifier', 'Decision Tree Classifier', 'K-means clustering', 'KNN clustering', 'TextBlob Classifier']
-    sizes = [bayes, dt, km, knn, txtblob]
+    labels = ['Naive Bayes Classifier', 'Decision Tree Classifier', 'K-means clustering', 'KNN clustering', 'Linear Regression', 'TextBlob Classifier']
+    sizes = [bayes, dt, km, knn, lr, txtblob]
 
     colors = np.random.rand(len(labels), 3)
     fig, ax = plt.subplots(figsize=(10, 10))
@@ -49,7 +49,7 @@ def barchart_models(bayes, dt, km, knn, txtblob):
     ax.bar(x_pos, sizes, color=colors)
 
     # Set the x-tick labels at each bar
-    ax.set_xticklabels(["Naive Bayes Classifier", "Decision Tree Classifier", "K-means clustering", "KNN clustering", "TextBlob Classifier"])
+    ax.set_xticklabels(["Naive Bayes Classifier", "Decision Tree Classifier", "K-means clustering", "KNN clustering", "Linear Regression", "TextBlob Classifier"])
     ax.set_xticks(x_pos)
 
     ax.set_title("Sentiment Score by Model")
