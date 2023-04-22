940f882d5b75f4f431af330488287bb6921a3b13
Jaime <jaime.deviverowoods2@mail.dcu.ie>
2023-04-22 12:04:31 +0100

    Frontend Features
    1. Added functions to get polarity in key dates
    2. Added data to key_dates.html, models.html, daily.html
    3. Updated lineplot.py to remove February 2020 from graph as no data for
       that month available
    4. Updated barchart and lineplot for all months

--- src/djangoProject/PandemicAnalyser/Plots/searcher.py
+++ src/djangoProject/PandemicAnalyser/Plots/searcher.py
@@ -6,7 +6,7 @@ def get_monthly_html_files(directory):
              "August 2020", "September 2020", "October 2020", "November 2020", "December 2020", "January 2021",
              "February 2021", "March 2021", "April 2021", "May 2021", "June 2021", "July 2021",
              "August 2021", "September 2021", "October 2021", "November 2021", "December 2021", "January 2022",
-             "Feb 2022", "March 2022", "April 2022", "May 2022", "June 2022", "July 2022",
+             "February 2022", "March 2022", "April 2022", "May 2022", "June 2022", "July 2022",
              "August 2022", "September 2022", "October 2022", "November 2022", "December 2022"]
 
     monthly_plots = [] # add all monthly plots, these will be ordered chronologically
